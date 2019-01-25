7c33d46d58565df573433275cb11849a1946c3ae
Thomas Cruz <tom.ortega47@gmail.com>
2019-01-25 17:06:42 +0800

    on the 3rd challenge, props. having problem with render

--- views/3_props_index.jsx
+++ views/3_props_index.jsx
@@ -20,15 +20,15 @@
     }
 
     class Todo extends React.Component {
-	  render(){
-	    return (
-	      <tr>
-	        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-	        <td style={{border: "1px solid black"}}>{this.props.children}</td>
-	      </tr>
-	    );
-	  }
-	}
+      render(){
+      	return(
+      		<tr>
+      			<td style ="border:1px solid black;">{this.props.title}</td>
+      			<td style ="border:1px solid black;">{this.props.children}</td>
+      		</tr>
+      	);
+      }
+    }
 
     class TodoForm extends React.Component {
       // Omitted
